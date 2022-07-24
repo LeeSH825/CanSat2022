6a9d2fc72a4fba1fe4a3d666246094f4f343c9a2
LeeSH825 <rubilsh@naver.com>
2022-07-24 22:43:24 +0900

    add stereo-disparity basic with VPI

---
+++ SW_CANSAT/VPI-disparity/depth_pipeline_cpp/build/CMakeFiles/depth_pipeline.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jetson/jetson-stereo-depth/detph_pipeline_cpp/main.cpp" "/home/jetson/jetson-stereo-depth/detph_pipeline_cpp/build/CMakeFiles/depth_pipeline.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "NV_VPI_VERSION_API=2000000"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/nvidia/vpi2/include"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SW_CANSAT/VPI-disparity/depth_pipeline_cpp/build/CMakeFiles/depth_pipeline.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/depth_pipeline.dir/main.cpp.o"
+  "depth_pipeline"
+  "depth_pipeline.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/depth_pipeline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
